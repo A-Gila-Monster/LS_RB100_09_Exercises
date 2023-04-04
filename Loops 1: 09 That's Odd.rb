6f0c09505433c82dbd750d32186e255770125b84
Gila Monster <gilamonster@gmail.com>
2023-04-04 01:09:02 +0000

    Miscellaneous updates to "Loops 1" exercises

---
+++ Loops 1: 09 That's Odd.rb
@@ -0,0 +1,13 @@
+# The code below shows an example of a for loop. Modify the code so that it only outputs i if i is an odd number.
+
+=begin
+
+for i in 1..100
+  puts i if i%2==1
+end
+
+=end
+
+for i in 1..100
+  puts i if i.odd?
+end
