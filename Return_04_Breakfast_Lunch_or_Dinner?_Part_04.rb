a397443e469210e66c75c06c278f85e5f9d63bea
Gila Monster <gilamonster@gmail.com>
2023-05-20 00:30:37 +0000

    RB100 basics exercises from "Return" section

---
+++ Return_04_Breakfast_Lunch_or_Dinner?_Part_04.rb
@@ -0,0 +1,10 @@
+# What will the following code print? Why? Don't run it until you've attempted to answer.
+
+def meal
+  puts 'Dinner'
+  return 'Breakfast'
+end
+
+puts meal
+
+# Should puts "Dinner", then "Breakfast"
---
+++ Return_10_Tricky_Number.rb
@@ -0,0 +1,13 @@
+# What will the following code print? Why? Don't run it until you've attempted to answer.
+
+def tricky_number
+  if true
+    number = 1
+  else
+    2
+  end
+end
+
+puts tricky_number
+
+# Returns 1 because variable assignment returns the value assigned to the variable!
