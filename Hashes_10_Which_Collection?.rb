2259e0d2891b4d6601c1712647acebb22600e859
Gila Monster <gilamonster@gmail.com>
2023-06-21 02:16:23 +0000

    Hashes exercises

---
+++ Hashes_10_Which_Collection?.rb
@@ -0,0 +1,12 @@
+# Rewrite car as a nested array containing the same key-value pairs.
+
+=begin
+car = {
+  type:  'sedan',
+  color: 'blue',
+  year:  2003
+}
+=end
+
+car = [[:type, 'sedan'], [:color, 'blue'], [:year,'2003']]
+p car
