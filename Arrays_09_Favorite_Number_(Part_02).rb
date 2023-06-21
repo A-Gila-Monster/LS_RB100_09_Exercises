3c5424b578c8323b1dfaca7a6dc38c83aff0b379
Gila Monster <gilamonster@gmail.com>
2023-06-21 01:42:24 +0000

    Arrays section of exercises

---
+++ Arrays_09_Favorite_Number_(Part_02).rb
@@ -0,0 +1,11 @@
+# In the code below, a nested array containing three groups of names and numbers is assigned to favorites.
+
+favorites = [['Dave', 7], ['Miranda', 3], ['Jason', 11]]
+favorites_flat = favorites.flatten
+p favorites_flat
+
+# Flatten and print this array.
+# That is, the printed result should not have any subarrays, but should have all of the original strings and numbers from the original array:
+
+# Expected output:
+# ["Dave", 7, "Miranda", 3, "Jason", 11]
